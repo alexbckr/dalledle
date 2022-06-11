4e8e84d10adb37be87be05ceb2f530e96cf9c0d3
alexbckr <amb3yn@virginia.edu>
2022-06-11 02:17:55 -0700

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'
+
+export default function Header(props) {
+    return (
+        <div className={styles.header}>
+           DALL-Edle
+        </div>
+    )
+}
