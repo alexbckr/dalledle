acbdbc9eedd8d62f619f5bafc6c5c12cb98b43bb
alexbckr <amb3yn@virginia.edu>
2022-06-12 02:25:25 -0700

    adding goolgle analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
 import '../styles/globals.css'
+import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
