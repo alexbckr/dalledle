90084fffefdddedb78b36b67375761618bb1b867
alexbckr <amb3yn@virginia.edu>
2022-06-12 19:57:26 -0700

    stats

--- components/StatsOverlay.js
+++ components/StatsOverlay.js
@@ -1,15 +1,25 @@
 import styles from '../styles/Overlays.module.css'
+import { useEffect, useState } from 'react'
 
 export default function StatsOverlay(props) {
+
+    const [stats, setStats] = useState([])
+
+    useEffect(() => {
+        var stats = localStorage.getItem("dalledle_statistics")
+        setStats(JSON.parse(stats))
+    }, [stats])
+
 
     return (
         <div className={styles.statsOverlayContainer}>
             <div className={styles.statsContainer}>
             <h1 className={styles.header}>Statistics</h1>
-            <p>Total:</p>
-            <p>Wins:</p>
-            <p>Win %:</p>
-            <p>Current Streak:</p>
+            <p>Games Played: {stats.gamesPlayed || 0}</p>
+            <p>Wins: {stats.gamesWon || 0}</p>
+            <p>Win %: {stats.winPercentage || 0}% </p>
+            <p>Current Streak: {stats.currentStreak || 0}</p>
+            <p>Best Streak: {stats.maxStreak || 0}</p>
             </div>
         </div>
     )
