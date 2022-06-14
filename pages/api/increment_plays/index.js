26d82cdf44e603e8dfe2ce2df551867fa1a66aed
alexbckr <amb3yn@virginia.edu>
2022-06-14 00:55:47 -0700

    add total solves

--- pages/api/increment_plays/index.js
+++ pages/api/increment_plays/index.js
@@ -1,14 +1,21 @@
-import { prisma } from '../../../lib/prisma.js';
+import { prisma } from "../../../lib/prisma.js"
 
 // PUT /api/increment_plays
 export default async function handle(req, res) {
-  const date = req.body.date;
+   const date = req.body.date
 
-  const stat = await prisma.image.update({
-    where: { date: date },
-    data: {
-        plays: {increment: 1}
-    },
-  });
-  res.json(stat);
+   const stat = await prisma.image.update({
+      where: { date: date },
+      data: {
+         plays: { increment: 1 },
+      },
+   })
+
+   const total_plays = await prisma.stat_book.update({
+      where: { id: "stat_book" },
+      data: {
+         total_plays: { increment: 1 },
+      },
+   })
+   res.json(stat)
 }
--- pages/api/increment_solves/index.js
+++ pages/api/increment_solves/index.js
@@ -1,14 +1,21 @@
-import { prisma } from '../../../lib/prisma.js';
+import { prisma } from "../../../lib/prisma.js"
 
 // PUT /api/increment_solves
 export default async function handle(req, res) {
-  const date = req.body.date;
+   const date = req.body.date
 
-  const stat = await prisma.image.update({
-    where: { date: date },
-    data: {
-        solves: {increment: 1}
-    },
-  });
-  res.json(stat);
+   const stat = await prisma.image.update({
+      where: { date: date },
+      data: {
+         solves: { increment: 1 },
+      },
+   })
+
+   const total_solves = await prisma.stat_book.update({
+      where: { id: "stat_book" },
+      data: {
+         total_solves: { increment: 1 },
+      },
+   })
+   res.json(stat)
 }
